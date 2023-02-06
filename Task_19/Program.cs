f1b8883c33836c68239c7c3fcf8c286909073d36
Davran_Djumanov <dda_82@mail.ru>
2023-02-06 09:14:21 +0600

    add task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine ("Введите пятизначное число: ");
+string num = Console.ReadLine();
+int pal = num.Length;
+
+if (pal == 5)
+{
+    if (num[0] == num[4] && num [1] == num[3])
+    {
+         Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(num + " --> Палиндром");
+    }
+
+    else
+    {
+         Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(num + " --> Не палиндром");
+    }
+}
+else
+{
+     Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine ($"Вы ввели НЕ ПЯТИЗНАЧНОЕ число.");
+    Console.WriteLine($"Введи правильное число !");
+}
+
